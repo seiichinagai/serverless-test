a96f4e8e74219e02fb0d1005f7da1688292216c1
seiichinagai <seiichinagai@gmail.com>
2021-10-11 08:59:44 -0700

    Updates

---
+++ precache-manifest.23a97461e8d55d108d88898eb58612b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e0d57e8548a48893d0e2565f72e65b",
+    "url": "/serverless-test/index.html"
+  },
+  {
+    "revision": "bdbbb351fec7e93ab7d5",
+    "url": "/serverless-test/static/css/main.96a7f1d5.chunk.css"
+  },
+  {
+    "revision": "1d8c66847a65232fa567",
+    "url": "/serverless-test/static/js/2.ab0878c8.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/serverless-test/static/js/2.ab0878c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdbbb351fec7e93ab7d5",
+    "url": "/serverless-test/static/js/main.35982dd8.chunk.js"
+  },
+  {
+    "revision": "e4daf586b317c7589fd3",
+    "url": "/serverless-test/static/js/runtime-main.e34943f8.js"
+  }
+]);
