9cd022a6b71c1274219d24422527e1af813f8a06
seiichinagai <seiichinagai@gmail.com>
2021-10-11 10:59:48 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Leave your message here:</h1>
                 <Form />
                 <Response ref={
                     Response => {
